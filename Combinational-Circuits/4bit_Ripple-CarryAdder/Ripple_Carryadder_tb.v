e0689453385b6376a1c81cbe42bd0c6a05d82769
Vaibhav Gunthe <vaibhavgunthe@gmail.com>
2025-06-25 13:58:36 +0530

    Update Ripple_Carryadder_tb.v

--- Combinational-Circuits/4bit_Ripple-CarryAdder/Ripple_Carryadder_tb.v
+++ Combinational-Circuits/4bit_Ripple-CarryAdder/Ripple_Carryadder_tb.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/22/2025 05:42:33 PM
-// Design Name: 
-// Module Name: Ripple_Carryadder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
 
